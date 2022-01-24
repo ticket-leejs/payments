ff962b59c49bffbf951850b3d47d2e3e6a088b5b
leejunsu <75758585@naver.com>
2022-01-24 22:25:41 +0900

    delete stripe

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -8,7 +8,6 @@ import {
   NotFoundError,
   OrderStatus,
 } from '@sgtickets/common';
-import { stripe } from '../stripe';
 import { Order } from '../models/order';
 import { Payment } from '../models/payment';
 import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
@@ -36,14 +35,9 @@ router.post(
       throw new BadRequestError('Cannot pay for an cancelled order');
     }
 
-    const charge = await stripe.charges.create({
-      currency: 'usd',
-      amount: order.price * 100,
-      source: token,
-    });
     const payment = Payment.build({
       orderId,
-      stripeId: charge.id,
+      stripeId: token,
     });
     await payment.save();
     new PaymentCreatedPublisher(natsWrapper.client).publish({
